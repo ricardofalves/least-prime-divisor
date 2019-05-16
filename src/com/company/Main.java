1c15dd0fcfc99f1d8bb8ce73baaafe09930233d8
Ricardo Furtado <rfurtado@AC-000702-glanna.local>
2019-05-15 22:16:05 -0300

    Adicionando caso para divisor igual a 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,33 +3,35 @@ package com.company;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-class Principal {
+class Main {
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        String line;
-        boolean EndOfInput = true;
-        int n;
+//        Scanner input = new Scanner(System.in);
+//        String line;
+//        boolean EndOfInput = true;
+//        int n;
+//
+//
+//        while (EndOfInput) {
+//            line = input.nextLine();
+//
+//            n = Integer.parseInt(line);
+//
+//            if (n == 0||n<2||n>100000000) {
+//                EndOfInput = false;
+//            } else {
 
-
-        while (EndOfInput) {
-            line = input.nextLine();
-
-            n = Integer.parseInt(line);
-
-            if (n == 0||n<2||n>100000000) {
-                EndOfInput = false;
-            } else {
-
-                int resultado = menorDivisor(n);
+                int resultado = menorDivisor(90);
                 System.out.print(resultado);
-                line = input.nextLine();
+//                line = input.nextLine();
 
             }
-        }
-    }
+//        }
+//    }
 
     public static int menorDivisor(int numero) {
+        if (numero % 2 == 0)
+            return 2;
         for (int i = 3; i * i <= numero; i += 2) {
             if (numero % i == 0)
                 return i;
